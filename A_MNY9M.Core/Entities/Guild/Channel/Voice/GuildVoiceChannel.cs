761a1e9547856e94a873d22ab9554cf0863d419b
Nikita <nekitosik.pav@gmail.com>
2026-05-25 23:19:02 +0700

    feature: добавить конфигурацию моделей

--- A_MNY9M.Core/Entities/Guild/Channel/Voice/GuildVoiceChannel.cs
+++ A_MNY9M.Core/Entities/Guild/Channel/Voice/GuildVoiceChannel.cs
@@ -4,6 +4,5 @@ namespace A_MNY9M.Core.Entities.Guild.Channel.Voice;
 
 public class GuildVoiceChannel : BaseGuildChannel
 {
-    public bool IsGeneratingChannel { get; set; }
     public bool IsTemporaryChannel { get; set; }
 }
